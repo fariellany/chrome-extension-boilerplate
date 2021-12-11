3b954aedf7d398073b09abd23404be7a7f8d4531
苏轼 <sushi.xiong@tuya.com>
2021-12-11 10:42:23 +0800

    build: commit lint

---
+++ .commitlintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional',
+  ],
+
+  // see：https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+  rules: {},
+};
